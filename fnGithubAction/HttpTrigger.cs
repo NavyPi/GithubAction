50af2a09c067d0d3418001e985aa5ae09316b3fa
Canafranca <a149367@bestbuy.com>
2021-03-15 09:23:35 -0500

    Add comment

--- fnGithubAction/HttpTrigger.cs
+++ fnGithubAction/HttpTrigger.cs
@@ -19,6 +19,7 @@ namespace fnGithubAction
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
+            //Extract name from query parameter
             string name = req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
